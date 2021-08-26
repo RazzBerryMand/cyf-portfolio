6f5072692c7db8ebe8a9c96f9d307a92902f4185
Mand Cashin <mandcashin@hotmail.co.uk>
2021-08-26 19:44:54 +0100

    feat: main section content added

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,11 +1,40 @@
 import React from "react";
-import './main.css';
+import "./main.css";
 
 export const Main = () => {
   return (
     <main>
-      <div>
-        Main component will go here
+      <div className="main-wrapper">
+        <div className="main-container">
+          <h2>About me</h2>
+          <p>
+            It has survived not only five centuries, but also the leap into
+            electronic typesetting, remaining essentially unchanged. It was
+            popularised in the 1960s with the release of Letraset sheets
+            containing Lorem Ipsum passages, and more recently with desktop
+            publishing software like Aldus PageMaker including versions of Lorem
+            Ipsum.
+          </p>
+        </div>
+        <div className="main-container bg-primary">
+          <h2>Technical skills</h2>
+          <ul>
+            <li>html</li>
+            <li>css</li>
+            <li>javascript</li>
+          </ul>
+        </div>
+        <div className="main-container">
+          <h2>Diversity</h2>
+          <p>
+            It has survived not only five centuries, but also the leap into
+            electronic typesetting, remaining essentially unchanged. It was
+            popularised in the 1960s with the release of Letraset sheets
+            containing Lorem Ipsum passages, and more recently with desktop
+            publishing software like Aldus PageMaker including versions of Lorem
+            Ipsum.
+          </p>
+        </div>
       </div>
     </main>
   );
