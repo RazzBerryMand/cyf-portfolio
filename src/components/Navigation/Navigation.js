2ffe0f31b2730d600ca3db9e949cf0cb9dac6dbe
Mand Cashin <mandcashin@hotmail.co.uk>
2021-08-26 19:12:41 +0100

    feat: navigation component

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,12 +1,30 @@
 import React from "react";
-import './navigation.css';
+import "./navigation.css";
 
 export const Navigation = () => {
-    return (
-        <nav>
-            <div>
-            Navigation component will go here
-            </div>
-        </nav>
-    );
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a
+            href="https://github.com/RazzBerryMand"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Github
+          </a>
+        </li>
+        <li>
+          <a href="https://linkedin.com" target="_blank" rel="noreferrer">
+            Linkedin
+          </a>
+        </li>
+        <li>
+          <a href="https://codepen.com" target="_blank" rel="noreferrer">
+            Codepen
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
 };
