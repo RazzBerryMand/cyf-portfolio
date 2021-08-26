2317ceab4dd56fafcc045afe11c4598a0e4bd829
Mand Cashin <mandcashin@hotmail.co.uk>
2021-08-26 20:26:17 +0100

    feat: update about section

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -8,12 +8,7 @@ export const Main = () => {
         <div className="main-container">
           <h2>About me</h2>
           <p>
-            It has survived not only five centuries, but also the leap into
-            electronic typesetting, remaining essentially unchanged. It was
-            popularised in the 1960s with the release of Letraset sheets
-            containing Lorem Ipsum passages, and more recently with desktop
-            publishing software like Aldus PageMaker including versions of Lorem
-            Ipsum.
+            I am a front end developer from manchester. I like react and vue.
           </p>
         </div>
         <div className="main-container bg-primary">
