5e64b45c70291f781ebc9a57539141b54f03e47f
Mand Cashin <mandcashin@hotmail.co.uk>
2021-08-26 18:49:02 +0100

    feat: added some content and styles to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,13 @@
 import React from "react";
-import './header.css';
+import "./header.css";
 
 export const Header = () => {
-    return (
-        <header>
-            <div>
-            Header component will go here
-            </div>
-        </header>
-    );
+  return (
+    <header>
+      <div className="header-container">
+        <h1>Mand Cashin</h1>
+        <p>Front end developer portfolio</p>
+      </div>
+    </header>
+  );
 };
